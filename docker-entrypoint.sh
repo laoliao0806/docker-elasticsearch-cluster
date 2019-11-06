d1e9287c4312c1ca8da3163c4707b2f78f3f2d58
laoliao <laoliao0806@gmail.com>
2019-11-06 16:39:34 +0800

    调试

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,13 @@ sleep 10
 CURRENT_POD_NUM=$(nslookup ${SERVICE_NAME} | grep Address | sed '1d' | awk '{print $2}' | wc -l)
 [[ $DEBUG ]] && echo $(nslookup ${SERVICE_NAME})> ./logfile
 if [[ $CURRENT_POD_NUM -gt 1 ]];then
-    sed -i '$a\discovery.zen.ping.unicast.hosts' /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i '$a\cluster.initial_master_nodes' /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i '$a\discovery.seed_hosts' /usr/share/elasticsearch/config/elasticsearch.yml
     ip=$(nslookup ${SERVICE_NAME} | grep Address | sed '1d' | awk '{print $2}')
     ips=$(echo $ip | tr ' ' ',')
     [[ $DEBUG ]] && echo ${ip} >> ./logfile
-    sed -i "s/discovery.zen.ping.unicast.hosts*/discovery.zen.ping.unicast.hosts: [${ips}]/g" /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i "s/cluster.initial_master_nodes*/cluster.initial_master_nodes: [${ips}]/g" /usr/share/elasticsearch/config/elasticsearch.yml
+    sed -i "s/discovery.seed_hosts*/discovery.seed_hosts: [${ips}]/g" /usr/share/elasticsearch/config/elasticsearch.yml
 fi
     
 [[ $PAUSE ]] && sleep $PAUSE
